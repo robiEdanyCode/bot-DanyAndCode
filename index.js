4dee6b5928df9038e98469743cd6644d269bd593
Daniel Brienza <roby3daniel@gmail.com>
2022-01-05 21:42:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,17 +3,6 @@ const client = new Discord.Client(
     {intents: ["GUILDS","GUILD_MESSAGES","GUILD_MEMBERS"]}
 )
 
-var checkBot = client.on("ready")
-
-client.on("ready", () =>{
-   console.log("Bot ONLINE!")
-})
-
-client.on("messageCreate", (message) => {
-    if(checkBot){
-        message.channel.send("Il bot è ONLINE!")
-    }
-    else{
-        message.channel.send("Il bot è OFFLINE!")
-    }
+client.on("ready", () => {
+    console.log("Bot ONLINE!")
 })
