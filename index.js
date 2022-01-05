b5856cd8f857c4209d9e3379fd9d8c4ad84e3eeb
Daniel Brienza <roby3daniel@gmail.com>
2022-01-05 21:47:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js')
-const client = new Discord.Client(
-    {intents: ["GUILDS","GUILD_MESSAGES","GUILD_MEMBERS"]}
+const client = new Discord(
+    {intents: ["GUILD_MEMEBERS","GUILDS","GUILD_MESSAGES"]}
 )
 
 client.on("ready", () => {
