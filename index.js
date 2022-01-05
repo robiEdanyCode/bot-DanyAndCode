d158b524bdef48e2b63740a3d0fd9c9a39c68957
Daniel Brienza <roby3daniel@gmail.com>
2022-01-05 21:59:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client(
     {intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MEMBERS"]}
 )
-client.login(process.env.token);
+client.login("OTI3NjUxMjU4MTA4NjM3MjQ0.YdNUjg.jzrGGEvZnZjhtWWZMeRNYisOEjo");
 
 client.on("ready", () => {
     console.log("Bot ONLINE")
