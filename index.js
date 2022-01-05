75476955c37109f8a6c565b42e484761dc317d2e
Daniel Brienza <roby3daniel@gmail.com>
2022-01-05 21:58:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
-const client = new Discord.client(
-    {intents: ["GUILDS","GUILD_MESSAGES","GUILD_MEMBERS"]}
+const client = new Discord.Client(
+    {intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MEMBERS"]}
 )
 client.login(process.env.token);
 
